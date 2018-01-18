4f48327e9a03fc01c8099cae532524b92d5b2a2a
Erwan CHESNAY <erwan.chesnay@etud.u-picardie.fr>
2018-01-19 00:17:52 +0100

    ajout IdFilm et NomFilm

--- index.php
+++ index.php
@@ -10,9 +10,24 @@ $client = new Client([
     'timeout'  => 2.0,
 ]);
 
-$response = $client->request('GET', 'theater');
-$data = json_decode($response->getBody());
+// Affichage pour le nom du cinema
 
+$CineName = $client->request('GET', 'theater');
+$data = json_decode($CineName->getBody());
+
+// Affichage pour les id films et titre
+
+$idShowtimes = $client->request('GET', 'showtimes');
+$dataId = json_decode($idShowtimes->getBody());
+
+foreach ($dataId as $key => $value) {
+  echo $value->id;
+  echo $value->name;
+  echo "<br>";
+}
+
+echo $dataId[0]->id;
+echo $dataId[0]->name;
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
