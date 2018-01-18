7ba2a4dbecdf85fdab5358801ec414752f640c5b
sowdowdow <simon.dormeau@etud.u-picardie.fr>
2018-01-18 19:57:44 +0100

    graphical improvements

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <div class="container">
-    <body>
-      <h1 class="display-1 text-center">Hello World</h1>
+    <body class="border border-danger rounded-circle">
+      <h1 class="display-1 text-center mt-5">Hello World</h1>
       <p class="text-center">ça part en steak</p>
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
