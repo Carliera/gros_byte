0b8a41d86cc8feb381c4febe5068df4bc8a4f33b
Kaïs Hadi <kais-khan.hadi@etud.u-picardie.fr>
2018-01-18 23:56:00 +0100

    /showtimes.php : ajout du name dans le tableau

--- showtimes.php
+++ showtimes.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/json');
-$json = [0 => ["id" => 423], 1 => ["id" => 424], 2=>["id"=>425]];
+
+$json = [0 => ["id" => 423, "name" => "Star Wars : Le reveil de la force"], 1 => ["id" => 424, "name" => "Les Cookies"], 2=>["id"=>425, "name" => "Megashark vs M.Lamperier"]];
 $json = json_encode($json);
 
 echo $json;
