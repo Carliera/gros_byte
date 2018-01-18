1d6b7193597c540399efe49332d71a0090a9d00b
Erwan CHESNAY <erwan.chesnay@etud.u-picardie.fr>
2018-01-18 22:55:08 +0100

    ptdr.json

--- index.php
+++ index.php
@@ -5,11 +5,13 @@ use GuzzleHttp\Client;
 
 $client = new Client([
     // Base URI is used with relative requests
-    'base_uri' => 'http://httpbin.org',
+    'base_uri' => 'http://80.211.175.238/',
     // You can set any number of default request options.
     'timeout'  => 2.0,
 ]);
 
-$response = $client->request('GET', 'test');
+$response = $client->request('GET', 'theater');
+$data = json_decode($response->getBody());
 
+echo $data->name;//->read(1024);
 ?>
