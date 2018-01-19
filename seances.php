281b1d96f55f91196ff4544fe02b5b75d12b099a
sowdowdow <simon.dormeau@etud.u-picardie.fr>
2018-01-19 01:38:15 +0100

    fix favicon

--- seances.php
+++ seances.php
@@ -26,7 +26,7 @@ $dataId = json_decode($idShowtimes->getBody());
 <head>
 <title>Le Grand Bô Técran</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="icon" href="image/favicon.png">
+<link rel="icon" href="image/favicon.ico">
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 <script src="js/jquery-1.4.2.min.js" type="text/javascript"></script>
 <script src="js/cufon-yui.js" type="text/javascript"></script>
