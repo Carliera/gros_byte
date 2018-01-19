c6f01b1030a144aa55980694878bd588edcc2aae
JOVER Vincent <vincent.jover@sfr.fr>
2018-01-19 02:39:43 +0100

    ajout de la page ajout d'une seance

---
+++ addseance.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Le Grand Bô Técran</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+<script src="js/jquery-1.4.2.min.js" type="text/javascript"></script>
+<script src="js/cufon-yui.js" type="text/javascript"></script>
+<script src="js/cufon-replace.js" type="text/javascript"></script>
+<script src="js/Gill_Sans_400.font.js" type="text/javascript"></script>
+<script src="js/script.js" type="text/javascript"></script>
+</head>
+<body id="page1">
+
+<div class="tail-top">
+  <div class="tail-bottom">
+    <div id="main">
+      <div id="header">
+        <div class="row-1">
+          <div class="fleft"><a href="#"><span>L</span>e<span>G</span>rand<span>B</span>ô<span>T</span>écran</a></div>
+          <ul>
+            <li><a href="#"><img src="images/icon1-act.gif" alt="" /></a></li>
+            <li><a href="#"><img src="images/icon2.gif" alt="" /></a></li>
+            <li><a href="#"><img src="images/icon3.gif" alt="" /></a></li>
+          </ul>
+        </div>
+        <div class="row-2">
+          <ul>
+            <li><a href="index.php" >Home</a></li>
+            <li><a href="seance.php" >Séances</a></li>
+            <li><a href="addseance.php" class="active">Ajouter une seance</a></li>
+            <li><a href="contact-us.html">Contacts</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+   </div>
+ </div>
+
+ <div class="content">
+   <h3>Ajouter une seance</h3>
+   <form id="contacts-form" action="#">
+     <fieldset>
+
+       <div class="field">
+         <label>ajout : </label>
+         <form action="cible.php" method="post">
+
+           <p>
+             <input type="text" name="prenom" />
+             <input type="submit" value="Valider" />
+           </p>
+         </form>
+
+       </div>
+
+     </fieldset>
+   </form>
+ </div>
+
+
+
+
+      <div id="footer">
+        <div class="left">
+          <div class="right">
+            <div class="footerlink">
+              <p class="lf">Copyright &copy; 2018 <a href="#">LeGrandBôTécran</a> - All Rights Reserved</p>
+              <p class="rf">Design by Les Gros Byte</p>
+              <div style="clear:both;"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript"> Cufon.now(); </script>
+<!-- END PAGE SOURCE -->
+</body>
+</html>
